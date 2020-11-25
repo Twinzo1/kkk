219813ed518137fe24711de4ceedfafecc5d8b63
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2020-11-25 19:35:05 +0800

    Update oneByOneExecute.js

--- oneByOneExecute.js
+++ oneByOneExecute.js
@@ -36,7 +36,7 @@ async function executeOneByOne() {
     for (var i = 0; i < CookieJDs.length; i++) {
         await requireConfig();
         await downFile();
-        let content = await fs.readFileSync(JSPath, 'utf8')
+        var content = await fs.readFileSync(JSPath, 'utf8')
         console.log(`正在执行第${i + 1}个账号签到任务`);
         await changeFiele(content, CookieJDs[i]);
         console.log("替换变量完毕");
