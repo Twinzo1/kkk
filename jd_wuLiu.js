a526fda5bfef366fd92d508266140e2bfadfe739
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2020-11-29 18:12:46 +0800

    Update jd_wuLiu.js

--- jd_wuLiu.js
+++ jd_wuLiu.js
@@ -43,7 +43,6 @@ $.getData('CookieJD2') && cookies.push($.getData('CookieJD2'));
 function requireConfig() {
   return new Promise(resolve => {
     console.log('开始获取配置文件\n')
-    notify = $.isNode() ? require('./sendNotify') : '';
     //Node.js用户请在jdCookie.js处填写京东ck;
     const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
     //IOS等用户直接用NobyDa的jd cookie
