9e03ab3b39a81df88a72e20a060815d98b3dcbd9
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2020-12-05 16:46:40 +0800

    Update comeAlong.js

--- comeAlong.js
+++ comeAlong.js
@@ -114,10 +114,11 @@ async function start() {
 	    while (new Date().getTime() < start + delay);
     }
     let waiting_time = 0;
-    if (Secrets.SyncUrl.search("j_xtg") != -1) {	
+    if (Secrets.SyncUrl.search("j_xtg") != -1) {
+    	console.log(Secrets.SyncUrl)
     	waiting_time = startTime()
     }
-    
+    return
     if (waiting_time <= 300000) {
         console.log("检测到离零点只有不到五分钟，脚本将等待" + waiting_time / 1000 + "s，到零点再执行");
         sleep(waiting_time);
