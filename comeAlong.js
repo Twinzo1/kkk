eb157e8c0a6e0212598d3fd902748a374a225abf
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2020-12-06 16:12:55 +0800

    Update comeAlong.js

--- comeAlong.js
+++ comeAlong.js
@@ -120,7 +120,7 @@ async function start() {
     	waiting_time = startTime()
     }
 
-    if (waiting_time <= 300000) {
+    if (waiting_time >= -120000 && waiting_time <= 300000) {
         console.log("检测到离零点只有不到五分钟，脚本将等待" + waiting_time / 1000 + "s，到零点再执行");
         sleep(waiting_time);
     } else {
