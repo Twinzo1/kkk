4199eb1954eb252a2efc785917133c8a7ef348a6
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2020-12-20 18:57:08 +0800

    Update jd_shareCodes_boarding.js

--- jd_shareCodes_boarding.js
+++ jd_shareCodes_boarding.js
@@ -14,7 +14,7 @@ const shareCodeMap = {
   pet: shareCodes.map(it => it.pet),
   ddfactory: shareCodes.map(it => it.ddfactory),
   jxfactory: shareCodes.map(it => it.jxfactory),
-  jdzz: shareCodes.map(it => it.jdzz),
+  jdzz: shareCodes.map(it => it.jdzz)
 }
 
 const SHARE_CODE_DESC = {
@@ -23,7 +23,7 @@ const SHARE_CODE_DESC = {
   'pet': '东东萌宠',
   'ddfactory': '东东工厂',
   'jxfactory': '京喜工厂',
-  'jdzz': '京东赚赚',
+  'jdzz': '京东赚赚'
 }
 
 const taskurl = (url) => {
